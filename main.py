aca02b67ecbaaef87365e61f5f67fd8c8dffb4d5
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-12-11 21:50:59 +1300

    Added display and added unique

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sqlite3
 
 
 def create_items_table(conn):
+    """
+    Creates the items table in sqlite if it
+    does not exist already
+    """
     sql_command = """ CREATE TABLE IF NOT EXISTS items (
                             id integer PRIMARY KEY,
-                            name text NOT NULL,
+                            name text NOT NULL UNIQUE,
                             quantity integer
                         );"""
     cursor = conn.cursor()
@@ -15,6 +19,9 @@ def create_items_table(conn):
 
 
 def create_item_tuple():
+    """ 
+    Takes in user input and returns a tuple of an item
+    """
     user_input = input("Enter item (name quantity): ")
     item = tuple(user_input.split())
     if len(item) == 2:
@@ -24,6 +31,9 @@ def create_item_tuple():
 
 
 def insert_item(conn, item):
+    """
+    Inserts item in tuple format to database
+    """
     sql_command = """INSERT INTO items (name, quantity)
                     VALUES(?, ?)"""
     cursor = conn.cursor()
@@ -31,10 +41,24 @@ def insert_item(conn, item):
     conn.commit()
 
 
+def display(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM items ORDER BY name")
+    rows = cursor.fetchall()
+    print("=" * 28)
+    print(f"{'Name':<15}{'Quantity':<10}")
+    for row in rows:
+        name = row[1]
+        quantity = row[2]
+        print(f"{name:<15}{quantity:<10}")
+
+
 def main():
     db_file = "./order.db"
     conn = sqlite3.connect(db_file)
     create_items_table(conn)
+    display(conn)
+    input()
     insert_item(conn, create_item_tuple())
 
 
