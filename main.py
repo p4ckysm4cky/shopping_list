7f15e5d01049502e147ff931d97d3be1f97270db
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-12-11 18:38:00 +1300

    Added item insertion

--- main.py
+++ main.py
@@ -14,10 +14,28 @@ def create_items_table(conn):
         print(e)
 
 
+def create_item_tuple():
+    user_input = input("Enter item (name quantity): ")
+    item = tuple(user_input.split())
+    if len(item) == 2:
+        return item
+    else:
+        print("Too many arguments provided to item")
+
+
+def insert_item(conn, item):
+    sql_command = """INSERT INTO items (name, quantity)
+                    VALUES(?, ?)"""
+    cursor = conn.cursor()
+    cursor.execute(sql_command, item)
+    conn.commit()
+
+
 def main():
     db_file = "./order.db"
     conn = sqlite3.connect(db_file)
     create_items_table(conn)
+    insert_item(conn, create_item_tuple())
 
 
 if __name__ == "__main__":
