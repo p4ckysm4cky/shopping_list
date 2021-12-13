f20401b7ed8b6cf57cb23332121c8ddde7aff989
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-12-13 15:09:47 +1300

    Added menu

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def create_item_tuple():
     """ 
     Takes in user input and returns a tuple of an item
     """
-    user_input = input("Enter item (name quantity): ")
-    item = tuple(user_input.split())
+    user_input = input("Enter item in format (name, quantity): ")
+    item = tuple(user_input.split(','))
     if len(item) == 2:
         return item
     else:
@@ -79,15 +79,45 @@ def display(conn):
         name = row[1]
         quantity = row[2]
         print(f"{name:<15}{quantity:<10}")
+    print("=" * 28)
+    print()
+
+
+def menu(conn):
+    print("""Please choose the following options:
+    1: Add new item to list
+    2: Delete item from list
+    3: Update item quantity
+    99: Exit""")
+    user_selection = input("").strip()
+    if user_selection == '1':
+        print("Insertion| ", end="")
+        insert_item(conn, create_item_tuple())
+        return 1
+    elif user_selection == '2':
+        item_name = input("Please enter the name of item you wish to delete: ")
+        delete_item(conn, item_name)
+        return 2
+    elif user_selection == '3':
+        print("Update| ", end="")
+        update_item_quantity(conn, *create_item_tuple())
+        return 3
+    elif user_selection == '99':
+        return 99
+    else:
+        print("Invalid option was chosen")
+        return 0
 
 
 def main():
     db_file = "./order.db"
     conn = sqlite3.connect(db_file)
-    create_items_table(conn)
+
     display(conn)
-    input()
-    insert_item(conn, create_item_tuple())
+    user_option = menu(conn)
+    while user_option != 99:
+        display(conn)
+        user_option = menu(conn)
 
 
 if __name__ == "__main__":
